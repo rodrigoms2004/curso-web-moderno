e3cf1e1ba9a7de9331c94d9bb75ddeb0feb3deab
Rodrigo <rodrigoms2004@gmail.com>
2018-07-02 17:07:38 -0300

    esnext capitulo final

---
+++ esnext/usandoPromises.js
@@ -0,0 +1,48 @@
+// aula 150
+// http://files.cod3r.com.br/curso-js/turmaA.json
+// http://files.cod3r.com.br/curso-js/turmaB.json
+// http://files.cod3r.com.br/curso-js/turmaC.json
+// com promise...
+const http = require('http')
+
+const getTurma = letra => {
+    const url = `http://files.cod3r.com.br/curso-js/turma${letra}.json`
+    return new Promise((resolve, reject) => {
+        http.get(url , res => {
+            let resultado = ''
+    
+            res.on('data', dados => {
+                resultado += dados
+            })
+    
+            res.on('end', () => {
+                try {
+                    resolve(JSON.parse(resultado))
+                } catch(e) {
+                    reject(e)
+                }
+            })
+        })    
+    })
+}
+
+// let nomes = []
+// getTurma('A').then(alunos => {
+//     nomes = nomes.concat(alunos.map(a => `A: ${a.nome}`))
+//     getTurma('B').then(alunos => {
+//         nomes = nomes.concat(alunos.map(a => `B: ${a.nome}`))
+//         getTurma('C').then(alunos => {
+//             nomes = nomes.concat(alunos.map(a => `C: ${a.nome}`))
+//             console.log(nomes)    
+//         })
+//     })
+// })
+
+Promise.all([getTurma('A'), getTurma('B'), getTurma('C')])
+    .then(turmas => [].concat(...turmas))               // concatena tudo em um único array
+    .then(alunos => alunos.map(aluno => aluno.nome))    // pega somente o nome dos alunos
+    .then(x => console.log(x))                          // imprime o array de nomes
+
+
+// gerando um erro
+getTurma('D').catch(e => console.log(e.message))        // Unexpected token < in JSON at position 0
