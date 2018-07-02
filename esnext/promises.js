61934d4c5a9d190b392457f2abae6febc11f4b74
Rodrigo <rodrigoms2004@gmail.com>
2018-07-02 16:01:35 -0300

    promises aula 148

---
+++ esnext/promises.js
@@ -0,0 +1,14 @@
+// resolve aceita somente um parametro
+function falarDepoisDe(segundos, frase) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(frase)
+        }, segundos * 1000)
+    })
+}
+
+falarDepoisDe(3, 'Que legal!')
+    .then(frase => frase.concat('?!?!'))
+    .then(outraFrase => console.log(outraFrase))
+    .catch(e => console.log(e)) 
+
