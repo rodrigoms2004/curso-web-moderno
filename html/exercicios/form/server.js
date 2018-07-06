215cccb8a53f192406579b168d5c89bc2304c04e
Rodrigo <rodrigoms2004@gmail.com>
2018-07-06 13:21:18 -0300

    tabela e forms

---
+++ html/exercicios/form/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/usuarios', (req, res) => {
+    console.log(req.body)   // {}
+    console.log(req.query)
+    res.send('<h1>Parabéns. Usuário Incluido!</h1>')
+})
+
+app.post('/usuarios', (req, res) => {
+    console.log(req.body)   // { nome: 'teste' } vem de 'name' do formulário
+    res.send('<h1>Parabéns. Usuário Incluido!</h1>')
+})
+
+app.post('/usuarios/:id', (req, res) => {
+    console.log(req.params.id)
+    console.log(req.body)   
+    res.send('<h1>Parabéns. Usuário Alterado!</h1>')
+})
+
+app.listen(3003)
