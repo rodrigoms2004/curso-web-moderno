855649de2770c16f14a01a54aa4918239a2b03e2
Rodrigo <rodrigoms2004@gmail.com>
2019-02-01 10:31:18 -0200

    Aulas 333, 334 React

---
+++ react/exercicios/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Primeiro from './componentes/Primeiro'
+
+
+ReactDOM.render(<Primeiro />, document.getElementById('root'))
+
+
+// const elemento = <h1>React 2</h1> 
+// ReactDOM.render(elemento, document.getElementById('root'))
