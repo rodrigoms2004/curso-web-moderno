217b6e2b4e0a1e599baf1f6267f1753696970203
Rodrigo <rodrigoms2004@gmail.com>
2019-02-01 11:07:57 -0200

    Aula 336 Multiplos componentes

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import BomDia from './componentes/BomDia'
+// import BomDia from './componentes/BomDia'
+import Multi, { BoaNoite } from './componentes/Multiplos'
 
 
-ReactDOM.render(<BomDia nome="Guilherme" idade={40}/>, document.getElementById('root'))
+ReactDOM.render(
+    <div>
+        <Multi.BoaTarde nome="Ana" />
+        <BoaNoite nome="Bia" />
+    </div>
+, document.getElementById('root'))
 
+
+// ReactDOM.render(<BomDia nome="Guilherme" idade={40}/>, document.getElementById('root'))
 // ou...
 // ReactDOM.render(<BomDia></BomDia>, document.getElementById('root'))
