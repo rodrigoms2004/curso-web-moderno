628287cfebdeebb4b7efb1a125970e698b3ef9e0
Rodrigo <rodrigoms2004@gmail.com>
2019-02-11 02:00:04 -0200

    Aula 358 React Router - Rotas

--- react/crud/frontend/src/main/App.jsx
+++ react/crud/frontend/src/main/App.jsx
@@ -1,15 +1,37 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 import React from 'react'
 
+// import { HashRouter } from 'react-router-dom'
+// http://localhost:3000/#/ HashRouter trabalha com #, menos problemas em produção
+// BrowserRouter não usa #, mais simples, mas pode ter problemas na produção com 
+// Apache ou nginX
+
+import { BrowserRouter } from 'react-router-dom'
+
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
-import Main from '../components/template/Main'
+import Routes from './Routes'
 import Footer from '../components/template/Footer'
 
 export default props => 
-    <div className="app">
-        <Logo />
-        <Nav />
-        <Main />
-        <Footer />
-    </div>
+    <BrowserRouter>
+        <div className="app">
+            <Logo />
+            <Nav />
+            <Routes />
+            <Footer />
+        </div>
+    </BrowserRouter>
+
+
+// export default props => 
+//     <HashRouter>
+//         <div className="app">
+//             <Logo />
+//             <Nav />
+//             <Routes />
+//             <Footer />
+//         </div>
+//     </HashRouter>
